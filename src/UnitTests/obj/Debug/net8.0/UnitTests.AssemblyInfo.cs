97efb68d3b29783661a101407f727d29b4d6d33b
MaxHotiuk <hotiukmax@gmail.com>
2024-07-11 16:22:18 +0300

    --

--- src/UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ src/UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2423ceb25ebd12ba16cb94b8d7208e0a3633e06b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3263efc2d5f194628e72b59bde57e95681a1db6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
